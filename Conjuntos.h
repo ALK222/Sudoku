15b174f0606a72574d837d4f94af71b377f2fad3
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2019-06-25 20:40:42 +0200

    Update Conjuntos.h

--- Conjuntos.h
+++ Conjuntos.h
@@ -1,4 +1,4 @@
-/*Programa de Alejandro Barrachina y Airam Martín
+/*Programa de Alejandro Barrachina
 Practica 2: Sudokus*/
 #pragma once
 #include "Header.h"
@@ -19,7 +19,7 @@ typedef struct {
 void cjto_vacio(tConjunto &c); //inicializa cjto c al vacio
 void cjto_lleno(tConjunto &c); //inicializa c con todos los valores a 1
 bool pertenece(const tConjunto &c, int e); //Devuelve si e(numero entero entre [1,9]) pertenece a c
-void annadeElemento(tConjunto &c, int e); //Añade un elemento e(entero entre [1,9]) al conjunto
+void annadeElemento(tConjunto &c, int e); //AÃ±ade un elemento e(entero entre [1,9]) al conjunto
 void borrarElemento(tConjunto &c, int e); //Borrar un elemento e(entero entre [1,9]) al conjunto
 int numElems(tConjunto &c);//Devuelve el numero de elementos que tiene c
 bool esUnitario(const tConjunto &c, int &e); //Dice si el cjto tiene un solo valor y lo devuelve
