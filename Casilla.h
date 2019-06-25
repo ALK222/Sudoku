6e990fb0992124b4ea994520dc49b93f2d67b2c7
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2019-06-25 20:39:21 +0200

    Update Casilla.h

--- Casilla.h
+++ Casilla.h
@@ -1,4 +1,4 @@
-/*Programa creado por Alejandro Barrachina y Airam Martín
+/*Programa creado por Alejandro Barrachina
 Practica 2: sudokus*/
 #pragma once
 #include "Conjuntos.h"
@@ -16,9 +16,9 @@ typedef struct {
 
 
 //Zona de inclusiones 
-void iniciaCasilla(tCasilla &casilla);// inicializa el estado de la casilla dada a vacío y su conjunto de valores posibles al conjunto{ 1,2,3,4,5,6,7,8,9 }
-void rellenaCasilla(tCasilla &casilla, char c, bool fija);/*rellena el estado y el nº de casilla de acuerdo con los valores de fija 
-y c.Si c es el carácter espacio en blanco se tratará de una casilla vacía; si c['0', '9'] el nº que almacena esa casilla será el entero
-equivalente(c–'0')y fija indicará si se trata de una casilla fija(valor true para fija) o rellenada(valor por defecto false).*/
+void iniciaCasilla(tCasilla &casilla);// inicializa el estado de la casilla dada a vacÃ­o y su conjunto de valores posibles al conjunto{ 1,2,3,4,5,6,7,8,9 }
+void rellenaCasilla(tCasilla &casilla, char c, bool fija);/*rellena el estado y el nÂº de casilla de acuerdo con los valores de fija 
+y c.Si c es el carÃ¡cter espacio en blanco se tratarÃ¡ de una casilla vacÃ­a; si c['0', '9'] el nÂº que almacena esa casilla serÃ¡ el entero
+equivalente(câ€“'0')y fija indicarÃ¡ si se trata de una casilla fija(valor true para fija) o rellenada(valor por defecto false).*/
 void dibujaCasilla(const tCasilla &casilla); //Dibuja cada casilla del sudoku
 bool esSimple(tCasilla &casilla, int &numero); //Devuelve si una casilla tiene un unico valor posible
